8db3b3e71a57f9e210bbab2472fb15e9c0b08166
vipintiwari685 <vipintiwari685@gmail.com>
2024-11-19 10:38:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { resolve } = require('path');
+let cors = require('cors');
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+let taxRate = 5;
+let discountPercentage = 10;
+let loyaltyRate = 2;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/cart-total', (req, res) => {
+  let newItemPrice = parseFloat(req.query.newItemPrice);
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let total = newItemPrice + cartTotal;
+  res.send(total.toString());
+});
+
+app.get('/membership-discount', (req, res) => {
+  let isMember = req.query.isMember;
+  let cartTotal = parseFloat(req.query.cartTotal);
+  if (isMember == 'true') {
+    cartTotal = cartTotal - (cartTotal * discountPercentage) / 100;
+  }
+  res.send(cartTotal.toString());
+});
+
+app.get('/calculate-tax', (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let tax = (cartTotal * taxRate) / 100;
+  res.send(tax.toString());
+});
+
+app.get('/estimate-delivery', (req, res) => {
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+  let eta;
+  if (shippingMethod == 'standard') {
+    eta = distance / 50;
+  } else {
+    eta = distance / 100;
+  }
+  res.send(eta.toString());
+});
+
+app.get('/shipping-cost', (req, res) => {
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+  let shippingCost = weight * distance * 0.1;
+  res.send(shippingCost.toString());
+});
+
+app.get('/loyalty-points', (req, res) => {
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+  let loyaltyPoints = purchaseAmount * loyaltyRate;
+
+  res.send(loyaltyPoints.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
